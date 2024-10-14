268b16d20804182ce8b5bf4729710c1cd597fa4b
sssgen <deskarte.des@gmail.com>
2024-10-14 18:09:13 +0300

    oop2 finished

--- classes/index.ts
+++ classes/index.ts
@@ -1,9 +1,23 @@
-abstract class Shapes {
-    abstract getArea(): number;
-    abstract getPerimeter(): number;
+export class CustomError {
+    public title: string;
+    public message: string;
+
+    constructor(error: CustomError) {
+        this.title = `**${error.title}`;
+        this.message = `${error.message}`;
+    }
 }
 
-class Squares extends Shapes {
+export abstract class ShapeClass {
+    abstract getArea(): number;
+    abstract getPerimeter(): number;
+
+    isAShape() {
+        return true;
+    }
+}
+
+export class SquareClass extends ShapeClass {
     private x1: number;
     private y1: number;
     private x2: number;
@@ -11,6 +25,9 @@ class Squares extends Shapes {
 
     constructor(x1: number, y1: number, x2: number, y2: number) {
         super();
+        if (Math.abs(x1 - x2) !== Math.abs(y1 - y2)) {
+            throw new CustomError({ title: "Неправильні значення", message: "Координати фігури мають формувати квадрат" });
+        }
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
@@ -18,29 +35,38 @@ class Squares extends Shapes {
     }
 
     getArea(): number {
-        return 2;
+        return Math.abs(this.x2 - this.x1) ** 2;
     }
+
     getPerimeter(): number {
-        return 3;
+        return 4 * Math.abs(this.x2 - this.x1);
+    }
+
+    getCoordinates() {
+        return `(${this.x1}; ${this.y1}), (${this.x2}; ${this.y2})`;
     }
 }
 
-class Circles extends Shapes {
+export class CircleClass extends ShapeClass {
     private x1: number;
     private y1: number;
     private radius: number;
 
     constructor(x1: number, y1: number, radius: number) {
         super();
+        if (radius <= 0) {
+            throw new CustomError({ title: "Неправильне значення", message: "Радіус має бути позитивним або більше за 0" });
+        }
         this.x1 = x1;
         this.y1 = y1;
-        this.radius = radius
+        this.radius = radius;
     }
 
     getArea(): number {
-        return 4;
+        return Math.abs(Math.PI * this.radius ** 2);
     }
+
     getPerimeter(): number {
-        return 5;
+        return Math.abs(2 * Math.PI * this.radius);
     }
 }
--- index.ts
+++ index.ts
@@ -1,11 +1,37 @@
-import { CustomError } from "./types";
+import {
+    SquareClass,
+    CircleClass,
+    ShapeClass,
+    CustomError
+} from "./classes";
 
 try {
-    const square = new Squares(2, 3, 4, 2);
-    const circle = new Circles(3, 3, 20);
+    const square = new SquareClass(3, 3, 2, 2);
+    const circle = new CircleClass(3, 3, 20);
+    // const errorSquare = new SquareClass(-3, 3, -20, 2);
+    // const errorCircle = new CircleClass(-3, 3, -20);
 
-    console.log(circle.getArea());
-} catch (e) {
-    console.error(`Some error occured: ${e}`);
+    let squareToShapes = square as ShapeClass;
+    let circleToShapes = circle as ShapeClass;
+
+    console.log(squareToShapes);
+    console.log(circleToShapes);
+
+    console.log("Тип квадрата: ", typeof square);
+    console.log("Чи є змінна 'квадрат' однією з класу квадратів? - ", square instanceof SquareClass);
+
+    console.log("Чи є квадрат фігурою? - " + square.isAShape());
+    console.log("Чи є коло фігурою? - " + circle.isAShape());
+
+    console.log(`Площа квадрата: ${square.getArea()}`)
+    console.log(`Площа кола: ${circle.getArea()}`)
+
+    console.log(`Координати квадрата: ${square.getCoordinates()}`)
+} catch (e: unknown) {
+    if (e instanceof CustomError) {
+        console.error(`[Помилка] ${e.title} - ${e.message}`);
+    } else {
+        console.error(`[Помилка] Непередбачувано виникла помилка: ${e}`);
+    }
 }
 
